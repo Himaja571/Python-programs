25a632dd0a825dde99be3831cef56ba320f499dd
Himaja Chanda <himajapatel0571@gmail.com>
2026-07-14 23:05:48 +0530

    Intial commit

---
+++ insertionsort.py
@@ -0,0 +1,23 @@
+# Insertion Sort
+
+n = int(input("Enter the number of elements: "))
+
+arr = []
+
+print("Enter the elements:")
+for i in range(n):
+    arr.append(int(input()))
+
+# Insertion Sort Logic
+for i in range(1, n):
+    key = arr[i]
+    j = i - 1
+
+    while j >= 0 and arr[j] > key:
+        arr[j + 1] = arr[j]
+        j -= 1
+
+    arr[j + 1] = key
+
+print("Sorted array:")
+print(arr)
