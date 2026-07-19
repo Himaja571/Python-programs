7227af5bc8e8a81a72623ea19658952b99c70bfd
Himaja Chanda <himajapatel0571@gmail.com>
2026-07-19 22:23:18 +0530

    Intial commit

---
+++ guessnumber.py
@@ -0,0 +1,14 @@
+import random
+
+secret = random.randint(1, 100)
+
+while True:
+    guess = int(input("Guess the number (1-100): "))
+
+    if guess < secret:
+        print("Too Low!")
+    elif guess > secret:
+        print("Too High!")
+    else:
+        print("Congratulations! You guessed correctly.")
+        break
