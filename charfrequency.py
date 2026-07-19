086ed20bfec9348f127eddfb945392aed6c07692
Himaja Chanda <himajapatel0571@gmail.com>
2026-07-19 22:21:27 +0530

    Intial commit

---
+++ charfrequency.py
@@ -0,0 +1,14 @@
+text = input("Enter a string: ")
+
+frequency = {}
+
+for ch in text:
+    if ch in frequency:
+        frequency[ch] += 1
+    else:
+        frequency[ch] = 1
+
+print("Character Frequencies:")
+
+for key, value in frequency.items():
+    print(key, ":", value)
