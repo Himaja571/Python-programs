a74f586aaf152c11695c8f00afc667a2e79351fa
Himaja Chanda <himajapatel0571@gmail.com>
2026-07-24 19:41:46 +0530

    Intial commit

---
+++ vowelsconsonants.py
@@ -0,0 +1,14 @@
+string = input("Enter a string: ")
+
+vowels = 0
+consonants = 0
+
+for ch in string.lower():
+    if ch.isalpha():
+        if ch in "aeiou":
+            vowels += 1
+        else:
+            consonants += 1
+
+print("Number of vowels:", vowels)
+print("Number of consonants:", consonants)
