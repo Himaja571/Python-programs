95322b57fc8fb3b46b3b6617d7f076dea8ac3f82
Himaja Chanda <himajapatel0571@gmail.com>
2026-09-11 22:39:13 +0530

    intial commit

---
+++ id.py
@@ -0,0 +1,6 @@
+employees=[101,102,103,104,105]
+search_id=104
+if search_id in employees:
+    print("Employee ID found:", search_id)
+else:
+    print("Employee ID not found.")
---
+++ sensor.py
@@ -0,0 +1,6 @@
+value=1
+while value <=5:
+    if value==4:
+        break
+    print("Sensor value:", value)
+    value+=1
